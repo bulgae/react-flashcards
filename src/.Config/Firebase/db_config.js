9d3267a63ee91c3ad65cf65ca550442d74252021
sejin choi <sejinchoi@sejindeMBP.hub>
2018-04-07 11:14:17 +1200

    Added firebase config

---
+++ src/.Config/Firebase/db_config.js
@@ -0,0 +1,10 @@
+import * as firebase from 'firebase'
+
+export const db_config = {
+    apiKey: "AIzaSyCc_f4IRGXwRhKSOLI3XFvzGrrC2bCKV9U",
+    authDomain: "https://flashcard-c918d.firebaseapp.com/",
+    databaseURL: "https://flashcard-c918d.firebaseio.com/",
+    storageBucket: "gs://flashcard-c918d.appspot.com",
+    messagingSenderId: "XXXXXXXXXXXX"
+
+}
